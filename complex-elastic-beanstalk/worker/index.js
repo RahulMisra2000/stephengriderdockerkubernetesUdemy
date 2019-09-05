40a8f6cdf3bbf7d83eb5066c2a624b09ee1d9a2d
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-09-05 00:00:29 -0400

    subscribing to event in redis

--- complex-elastic-beanstalk/worker/index.js
+++ complex-elastic-beanstalk/worker/index.js
@@ -13,7 +13,7 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fib(parseInt(message)));
+sub.on('message', (channel, message) => {                         // ********* writing the event handler foe when something is written to redis
+  redisClient.hset('values', message, fib(parseInt(message)));    // **** writing something back to redis
 });
-sub.subscribe('insert');
+sub.subscribe('insert');                                          // ********  we are subscribing to the insert event raised by redis
